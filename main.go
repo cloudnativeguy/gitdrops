7c153942cc8c11cd0cdc05ff0fef3e0b8536fb04
nolancon <conor.nolan@intel.com>
2021-05-12 17:13:01 +0100

    first auth test with os.Getenv token

---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+)
+
+func main() {
+	url := "https://api.digitalocean.com/v2/droplets"
+
+	// Create a Bearer string by appending string access token
+	var bearer = "Bearer " + os.Getenv("DO_TOKEN")
+
+	// Create a new request using http
+	req, err := http.NewRequest("GET", url, nil)
+
+	// add authorization header to the req
+	req.Header.Add("Authorization", bearer)
+
+	// Send req using http Client
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Println("Error on response.\n[ERROR] -", err)
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Println("Error while reading the response bytes:", err)
+	}
+	log.Println(string([]byte(body)))
+}
