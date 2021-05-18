a5bacaacc60dca3e8e14dfa58c3955a875b712ae
nolancon <conor.nolan@intel.com>
2021-05-18 17:21:16 +0100

    add schedule to repo with workflow on push - update to time

--- main.go
+++ main.go
@@ -4,22 +4,13 @@ import (
 	"context"
 	"time"
 
-	"github.com/nolancon/gitdrops/pkg/dolocal"
 	"github.com/nolancon/gitdrops/pkg/reconcile"
-
-	"github.com/digitalocean/godo"
 )
 
 const (
 	shortDuration    = 10 * time.Second
 	gitdropsYamlPath = "./gitdrops.yaml"
 )
-
-type ReconcileDroplets struct {
-	client                     *godo.Client
-	activeDroplets             []godo.Droplet
-	localDropletCreateRequests []dolocal.LocalDropletCreateRequest
-}
 
 func main() {
 	ctx, cancel := context.WithTimeout(context.Background(), shortDuration)
