091dba1bdcb1025153c608edb8f38b502b684455
nolancon <conor.nolan@intel.com>
2021-05-19 16:24:59 +0100

    modularise reconciler objects

---
+++ pkg/reconcile/droplets.go
@@ -0,0 +1,213 @@
+package reconcile
+
+import (
+	"context"
+	"errors"
+	"log"
+
+	"github.com/nolancon/gitdrops/pkg/dolocal"
+
+	"github.com/digitalocean/godo"
+)
+
+type DropletReconciler struct {
+	client                     *godo.Client
+	activeDroplets             []godo.Droplet
+	localDropletCreateRequests []dolocal.LocalDropletCreateRequest
+	dropletsToCreate           []dolocal.LocalDropletCreateRequest
+	dropletsToUpdate           actionsByID
+	dropletsToDelete           []int
+}
+
+func (dr *DropletReconciler) Populate(ctx context.Context) error {
+	activeDroplets, err := dolocal.ListDroplets(ctx, dr.client)
+	if err != nil {
+		log.Println("Error while listing droplets", err)
+		return err
+	}
+
+	dr.activeDroplets = activeDroplets
+	dr.ObjectsToUpdateAndCreate()
+	dr.ObjectsToDelete()
+
+	log.Println("active droplets:", len(activeDroplets))
+	log.Println("active droplets to delete:", dr.dropletsToDelete)
+	log.Println("gitdrops droplets to update:", dr.dropletsToUpdate)
+	log.Println("gitdrops droplets to create:", dr.dropletsToCreate)
+
+	return nil
+}
+
+func translateDropletCreateRequest(localDropletCreateRequest dolocal.LocalDropletCreateRequest) (*godo.DropletCreateRequest, error) {
+	createRequest := &godo.DropletCreateRequest{}
+	if localDropletCreateRequest.Name == "" {
+		return createRequest, errors.New("droplet name not specified")
+	}
+	if localDropletCreateRequest.Region == "" {
+		return createRequest, errors.New("droplet region not specified")
+	}
+	if localDropletCreateRequest.Size == "" {
+		return createRequest, errors.New("droplet size not specified")
+	}
+	if localDropletCreateRequest.Image == "" {
+		return createRequest, errors.New("droplet image not specified")
+	}
+	createRequest.Name = localDropletCreateRequest.Name
+	createRequest.Region = localDropletCreateRequest.Region
+	createRequest.Size = localDropletCreateRequest.Size
+	dropletImage := godo.DropletCreateImage{}
+	dropletImage.Slug = localDropletCreateRequest.Image
+	createRequest.Image = dropletImage
+
+	if len(localDropletCreateRequest.SSHKeyFingerprint) != 0 {
+		dropletCreateSSHKey := godo.DropletCreateSSHKey{Fingerprint: localDropletCreateRequest.SSHKeyFingerprint}
+		dropletCreateSSHKeys := make([]godo.DropletCreateSSHKey, 0)
+		dropletCreateSSHKeys = append(dropletCreateSSHKeys, dropletCreateSSHKey)
+		createRequest.SSHKeys = dropletCreateSSHKeys
+	}
+
+	if localDropletCreateRequest.VPCUUID != "" {
+		createRequest.VPCUUID = localDropletCreateRequest.VPCUUID
+	}
+
+	if len(localDropletCreateRequest.Volumes) != 0 {
+		dropletCreateVolumes := make([]godo.DropletCreateVolume, 0)
+		for _, vol := range localDropletCreateRequest.Volumes {
+			dropletCreateVolume := godo.DropletCreateVolume{ID: vol}
+			dropletCreateVolumes = append(dropletCreateVolumes, dropletCreateVolume)
+		}
+		createRequest.Volumes = dropletCreateVolumes
+	}
+	if len(localDropletCreateRequest.Tags) != 0 {
+		createRequest.Tags = localDropletCreateRequest.Tags
+	}
+	if localDropletCreateRequest.VPCUUID != "" {
+		createRequest.VPCUUID = localDropletCreateRequest.VPCUUID
+	}
+	if localDropletCreateRequest.UserData.Data != "" {
+		createRequest.UserData = localDropletCreateRequest.UserData.Data
+	}
+	return createRequest, nil
+
+}
+
+// dropletsToUpdateCreate poulates DropletReconciler with two lists:
+// * dropletsToUpdate: dropletActionsByID of droplets that are active on DO and are defined in
+// gitdrops.yaml, but the active droplets are no longer in sync with the local gitdrops version.
+// * dropletsToCreate: LocalDropletCreateRequests of droplets defined in gitdrops.yaml that are NOT
+// active on DO and therefore should be created.
+func (dr *DropletReconciler) ObjectsToUpdateAndCreate() {
+	dropletsToCreate := make([]dolocal.LocalDropletCreateRequest, 0)
+	dropletActionsByID := make(actionsByID)
+	for _, localDropletCreateRequest := range dr.localDropletCreateRequests {
+		dropletIsActive := false
+		for _, activeDroplet := range dr.activeDroplets {
+			if localDropletCreateRequest.Name == activeDroplet.Name {
+				//droplet already exists, check for change in request
+				log.Println("droplet found check for change")
+				// only do below check if delete privileges are granted
+				dropletActions := getDropletActions(localDropletCreateRequest, activeDroplet)
+				if len(dropletActions) != 0 {
+					dropletActionsByID[activeDroplet.ID] = dropletActions
+				}
+				dropletIsActive = true
+				continue
+			}
+		}
+		if !dropletIsActive {
+			//create droplet from local request
+			log.Println("droplet not active, create droplet ", localDropletCreateRequest)
+			dropletsToCreate = append(dropletsToCreate, localDropletCreateRequest)
+		}
+	}
+	dr.dropletsToUpdate = dropletActionsByID
+	dr.dropletsToCreate = dropletsToCreate
+}
+
+// ObjectToDelete populates DropletReconciler with  a list of IDs for droplets that need
+// to be deleted upon reconciliation of gitdrops.yaml (ie these droplets are active but not present
+// in the spec)
+func (dr *DropletReconciler) ObjectsToDelete() {
+	dropletsToDelete := make([]int, 0)
+
+	for _, activeDroplet := range dr.activeDroplets {
+		activeDropletInSpec := false
+		for _, localDropletCreateRequest := range dr.localDropletCreateRequests {
+			if localDropletCreateRequest.Name == activeDroplet.Name {
+				activeDropletInSpec = true
+				continue
+			}
+		}
+		if !activeDropletInSpec {
+			//create droplet from local request
+			dropletsToDelete = append(dropletsToDelete, activeDroplet.ID)
+		}
+	}
+	dr.dropletsToDelete = dropletsToDelete
+}
+
+func getDropletActions(localDropletCreateRequest dolocal.LocalDropletCreateRequest, activeDroplet godo.Droplet) []action {
+	var dropletActions []action
+	if activeDroplet.Size != nil && activeDroplet.Size.Slug != localDropletCreateRequest.Size {
+		log.Println("droplet (name)", activeDroplet.Name, " (ID)", activeDroplet.ID, " size has been updated in gitdrops.yaml")
+
+		dropletAction := action{
+			action: resize,
+			value:  localDropletCreateRequest.Size,
+		}
+		dropletActions = append(dropletActions, dropletAction)
+
+	}
+	if activeDroplet.Image != nil && activeDroplet.Image.Slug != localDropletCreateRequest.Image {
+		log.Println("droplet (name)", activeDroplet.Name, " (ID)", activeDroplet.ID, " image  has been updated in gitdrops.yaml")
+		dropletAction := action{
+			action: rebuild,
+			value:  localDropletCreateRequest.Image,
+		}
+		dropletActions = append(dropletActions, dropletAction)
+	}
+	return dropletActions
+}
+
+func (dr *DropletReconciler) DeleteObjects(ctx context.Context) error {
+	for _, id := range dr.dropletsToDelete {
+		err := dolocal.DeleteDroplet(ctx, dr.client, id)
+		if err != nil {
+			log.Println("error during delete request for droplet ", id, " error: ", err)
+			return err
+		}
+	}
+	return nil
+}
+
+func (dr *DropletReconciler) CreateObjects(ctx context.Context) error {
+	for _, dropletToCreate := range dr.dropletsToCreate {
+		dropletCreateRequest, err := translateDropletCreateRequest(dropletToCreate)
+		if err != nil {
+			log.Println("error converting gitdrops.yaml to droplet create request:")
+			return err
+		}
+		log.Println("dropletCreateRequest", dropletCreateRequest)
+		err = dolocal.CreateDroplet(ctx, dr.client, dropletCreateRequest)
+		if err != nil {
+			log.Println("error creating droplet ", dropletToCreate.Name)
+			return err
+		}
+
+	}
+	return nil
+}
+
+func (dr *DropletReconciler) UpdateObjects(ctx context.Context) error {
+	for id, dropletActions := range dr.dropletsToUpdate {
+		for _, dropletAction := range dropletActions {
+			err := dolocal.UpdateDroplet(ctx, dr.client, id, dropletAction.action, dropletAction.value)
+			if err != nil {
+				log.Println("error during action request for droplet ", id, " error: ", err)
+				// we do not return here as there may be more actions to complete
+				// for this droplet.
+			}
+		}
+	}
+	return nil
+}
--- pkg/reconcile/reconcile.go
+++ pkg/reconcile/reconcile.go
@@ -2,7 +2,6 @@ package reconcile
 
 import (
 	"context"
-	"errors"
 	"log"
 	"os"
 
@@ -19,52 +18,70 @@ const (
 	digitaloceanToken = "DIGITALOCEAN_TOKEN"
 )
 
-type ReconcileDroplets struct {
-	client         *godo.Client
-	gitDrops       dolocal.GitDrops
-	activeDroplets []godo.Droplet
+type ObjectReconciler interface {
 }
 
-type dropletActionsByID map[int][]dropletAction
+type Reconciler struct {
+	privileges        dolocal.Privileges
+	volumeReconciler  *VolumeReconciler
+	dropletReconciler *DropletReconciler
+}
 
-type dropletAction struct {
+type VolumeReconciler struct {
+	client                    *godo.Client
+	activeVolumes             []godo.Volume
+	localVolumeCreateRequests []dolocal.LocalVolumeCreateRequest
+	volumesToCreate           []dolocal.LocalVolumeCreateRequest
+	volumesToUpdate           actionsByID
+	volumesToDelete           []int
+}
+
+type actionsByID map[int][]action
+
+type action struct {
+	object string
 	action string
 	value  string
 }
 
-func NewReconcileDroplets(ctx context.Context) (ReconcileDroplets, error) {
+func NewReconciler(ctx context.Context) (Reconciler, error) {
 	gitDrops, err := dolocal.ReadGitDrops()
 	if err != nil {
 		log.Println(err)
-		return ReconcileDroplets{}, err
+		return Reconciler{}, err
 	}
 
 	client := godo.NewFromToken(os.Getenv(digitaloceanToken))
-	activeDroplets, err := dolocal.ListDroplets(ctx, client)
-	if err != nil {
-		log.Println("Error while listing droplets", err)
-		return ReconcileDroplets{}, err
+
+	//	activeVolumes, err := dolocal.ListVolumes(ctx, client)
+	//	if err != nil {
+	//		log.Println("Error while listing volumes", err)
+	//		return Reconciler{}, err
+	//	}
+
+	//	log.Println("active volumes on digitalocean:", len(activeVolumes))
+	//	volumeReconciler := &VolumeReconciler{
+	//		client:                    client,
+	//		activeVolumes:             activeVolumes,
+	//		localVolumeCreateRequests: gitDrops.Volumes,
+	//	}
+
+	dropletReconciler := &DropletReconciler{
+		client:                     client,
+		localDropletCreateRequests: gitDrops.Droplets,
 	}
+	err = dropletReconciler.Populate(ctx)
 
-	log.Println("active droplets on digitalocean:", len(activeDroplets))
-
-	return ReconcileDroplets{
-		client:         client,
-		gitDrops:       gitDrops,
-		activeDroplets: activeDroplets,
+	return Reconciler{
+		privileges: gitDrops.Privileges,
+		//		volumeReconciler:  volumeReconciler,
+		dropletReconciler: dropletReconciler,
 	}, nil
 }
 
-func (rd *ReconcileDroplets) Reconcile(ctx context.Context) error {
-	dropletsToUpdate, dropletsToCreate := rd.dropletsToUpdateCreate()
-	log.Println("active droplets to update", dropletsToUpdate)
-	log.Println("droplets to create ", dropletsToCreate)
-	dropletsToDelete := rd.activeDropletsToDelete()
-	log.Println("active droplets to delete ", dropletsToDelete)
-
-	dropletsToDelete = append(dropletsToDelete)
-	if rd.gitDrops.Privileges.Delete {
-		err := rd.deleteDroplets(ctx, dropletsToDelete)
+func (r *Reconciler) Reconcile(ctx context.Context) error {
+	if r.privileges.Delete {
+		err := r.dropletReconciler.DeleteObjects(ctx)
 		if err != nil {
 			log.Println("error deleting droplet")
 			return err
@@ -73,8 +90,8 @@ func (rd *ReconcileDroplets) Reconcile(ctx context.Context) error {
 		log.Println("gitdrops.yaml does not have delete privileges")
 	}
 
-	if rd.gitDrops.Privileges.Create {
-		err := rd.createDroplets(ctx, dropletsToCreate)
+	if r.privileges.Create {
+		err := r.dropletReconciler.CreateObjects(ctx)
 		if err != nil {
 			log.Println("error creating droplet")
 			return err
@@ -82,8 +99,8 @@ func (rd *ReconcileDroplets) Reconcile(ctx context.Context) error {
 	} else {
 		log.Println("gitdrops.yaml does not have create privileges")
 	}
-	if rd.gitDrops.Privileges.Update {
-		err := rd.updateDroplets(ctx, dropletsToUpdate)
+	if r.privileges.Update {
+		err := r.dropletReconciler.UpdateObjects(ctx)
 		if err != nil {
 			log.Println("error updating droplet")
 			return err
@@ -92,177 +109,5 @@ func (rd *ReconcileDroplets) Reconcile(ctx context.Context) error {
 		log.Println("gitdrops.yaml does not have update privileges")
 	}
 
-	return nil
-}
-
-func translateDropletCreateRequest(localDropletCreateRequest dolocal.LocalDropletCreateRequest) (*godo.DropletCreateRequest, error) {
-	createRequest := &godo.DropletCreateRequest{}
-	if localDropletCreateRequest.Name == "" {
-		return createRequest, errors.New("droplet name not specified")
-	}
-	if localDropletCreateRequest.Region == "" {
-		return createRequest, errors.New("droplet region not specified")
-	}
-	if localDropletCreateRequest.Size == "" {
-		return createRequest, errors.New("droplet size not specified")
-	}
-	if localDropletCreateRequest.Image == "" {
-		return createRequest, errors.New("droplet image not specified")
-	}
-	createRequest.Name = localDropletCreateRequest.Name
-	createRequest.Region = localDropletCreateRequest.Region
-	createRequest.Size = localDropletCreateRequest.Size
-	dropletImage := godo.DropletCreateImage{}
-	dropletImage.Slug = localDropletCreateRequest.Image
-	createRequest.Image = dropletImage
-
-	if len(localDropletCreateRequest.SSHKeyFingerprint) != 0 {
-		dropletCreateSSHKey := godo.DropletCreateSSHKey{Fingerprint: localDropletCreateRequest.SSHKeyFingerprint}
-		dropletCreateSSHKeys := make([]godo.DropletCreateSSHKey, 0)
-		dropletCreateSSHKeys = append(dropletCreateSSHKeys, dropletCreateSSHKey)
-		createRequest.SSHKeys = dropletCreateSSHKeys
-	}
-
-	if localDropletCreateRequest.VPCUUID != "" {
-		createRequest.VPCUUID = localDropletCreateRequest.VPCUUID
-	}
-
-	if len(localDropletCreateRequest.Volumes) != 0 {
-		dropletCreateVolumes := make([]godo.DropletCreateVolume, 0)
-		for _, vol := range localDropletCreateRequest.Volumes {
-			dropletCreateVolume := godo.DropletCreateVolume{ID: vol}
-			dropletCreateVolumes = append(dropletCreateVolumes, dropletCreateVolume)
-		}
-		createRequest.Volumes = dropletCreateVolumes
-	}
-	if len(localDropletCreateRequest.Tags) != 0 {
-		createRequest.Tags = localDropletCreateRequest.Tags
-	}
-	if localDropletCreateRequest.VPCUUID != "" {
-		createRequest.VPCUUID = localDropletCreateRequest.VPCUUID
-	}
-	if localDropletCreateRequest.UserData.Data != "" {
-		createRequest.UserData = localDropletCreateRequest.UserData.Data
-	}
-	return createRequest, nil
-
-}
-
-// dropletsToUpdateCreate returns two lists:
-// * dropletsToUpdate: dropletActionsByID of droplets that are active on DO and are defined in
-// gitdrops.yaml, but the active droplets are no longer in sync with the local gitdrops version.
-// * dropletsToCreate: LocalDropletCreateRequests of droplets defined in gitdrops.yaml that are NOT
-// active on DO and therefore should be created.
-func (rd *ReconcileDroplets) dropletsToUpdateCreate() (dropletActionsByID, []dolocal.LocalDropletCreateRequest) {
-	dropletsToCreate := make([]dolocal.LocalDropletCreateRequest, 0)
-	dropletActionsByID := make(dropletActionsByID)
-	for _, localDropletCreateRequest := range rd.gitDrops.Droplets {
-		dropletIsActive := false
-		for _, activeDroplet := range rd.activeDroplets {
-			if localDropletCreateRequest.Name == activeDroplet.Name {
-				//droplet already exists, check for change in request
-				log.Println("droplet found check for change")
-				// only do below check if delete privileges are granted
-				dropletActions := getDropletActions(localDropletCreateRequest, activeDroplet)
-				if len(dropletActions) != 0 {
-					dropletActionsByID[activeDroplet.ID] = dropletActions
-				}
-				dropletIsActive = true
-				continue
-			}
-		}
-		if !dropletIsActive {
-			//create droplet from local request
-			log.Println("droplet not active, create droplet ", localDropletCreateRequest)
-			dropletsToCreate = append(dropletsToCreate, localDropletCreateRequest)
-		}
-	}
-	return dropletActionsByID, dropletsToCreate
-}
-
-// activeDropletsToDelete return a list of IDs for droplets that need to be deleted upon reconciliation
-// of gitdrops.yaml (ie these droplets are active but not present in the spec)
-func (rd *ReconcileDroplets) activeDropletsToDelete() []int {
-	dropletsToDelete := make([]int, 0)
-
-	for _, activeDroplet := range rd.activeDroplets {
-		activeDropletInSpec := false
-		for _, localDropletCreateRequest := range rd.gitDrops.Droplets {
-			if localDropletCreateRequest.Name == activeDroplet.Name {
-				activeDropletInSpec = true
-				continue
-			}
-		}
-		if !activeDropletInSpec {
-			//create droplet from local request
-			dropletsToDelete = append(dropletsToDelete, activeDroplet.ID)
-		}
-	}
-	return dropletsToDelete
-}
-
-func getDropletActions(localDropletCreateRequest dolocal.LocalDropletCreateRequest, activeDroplet godo.Droplet) []dropletAction {
-	var dropletActions []dropletAction
-	if activeDroplet.Size != nil && activeDroplet.Size.Slug != localDropletCreateRequest.Size {
-		log.Println("droplet (name)", activeDroplet.Name, " (ID)", activeDroplet.ID, " size has been updated in gitdrops.yaml")
-
-		dropletAction := dropletAction{
-			action: resize,
-			value:  localDropletCreateRequest.Size,
-		}
-		dropletActions = append(dropletActions, dropletAction)
-
-	}
-	if activeDroplet.Image != nil && activeDroplet.Image.Slug != localDropletCreateRequest.Image {
-		log.Println("droplet (name)", activeDroplet.Name, " (ID)", activeDroplet.ID, " image  has been updated in gitdrops.yaml")
-		dropletAction := dropletAction{
-			action: rebuild,
-			value:  localDropletCreateRequest.Image,
-		}
-		dropletActions = append(dropletActions, dropletAction)
-	}
-	return dropletActions
-}
-
-func (rd *ReconcileDroplets) deleteDroplets(ctx context.Context, dropletsToDelete []int) error {
-	for _, id := range dropletsToDelete {
-		err := dolocal.DeleteDroplet(ctx, rd.client, id)
-		if err != nil {
-			log.Println("error during delete request for droplet ", id, " error: ", err)
-			return err
-		}
-	}
-	return nil
-}
-
-func (rd *ReconcileDroplets) createDroplets(ctx context.Context, dropletsToCreate []dolocal.LocalDropletCreateRequest) error {
-	for _, dropletToCreate := range dropletsToCreate {
-		dropletCreateRequest, err := translateDropletCreateRequest(dropletToCreate)
-		if err != nil {
-			log.Println("error converting gitdrops.yaml to droplet create request:")
-			return err
-		}
-		log.Println("dropletCreateRequest", dropletCreateRequest)
-		err = dolocal.CreateDroplet(ctx, rd.client, dropletCreateRequest)
-		if err != nil {
-			log.Println("error creating droplet ", dropletToCreate.Name)
-			return err
-		}
-
-	}
-	return nil
-}
-
-func (rd *ReconcileDroplets) updateDroplets(ctx context.Context, dropletsToUpdate dropletActionsByID) error {
-	for id, dropletActions := range dropletsToUpdate {
-		for _, dropletAction := range dropletActions {
-			err := dolocal.UpdateDroplet(ctx, rd.client, id, dropletAction.action, dropletAction.value)
-			if err != nil {
-				log.Println("error during action request for droplet ", id, " error: ", err)
-				// we do not return here as there may be more actions to complete
-				// for this droplet.
-			}
-		}
-	}
 	return nil
 }
