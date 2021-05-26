cea9ee53ae56ad6ceda9ced08cb797b5913f0cd5
nolancon <conor.nolan@intel.com>
2021-05-26 13:59:54 +0100

    tidy up from static analysis CI

--- main.go
+++ main.go
@@ -2,24 +2,11 @@ package main
 
 import (
 	"context"
-	"time"
 
 	"github.com/nolancon/gitdrops/pkg/reconcile"
 )
 
-const (
-	// 15s as there is a 10s sleep between reconciliations to allow updates
-	duration = 15 * time.Second
-)
-
 func main() {
-	//	var ctx context.Context
-	//	var ctxCancelFunc context.CancelFunc
-	//	var timeUntilContextDeadline = time.Now().Add(duration)
-	//
-	//	ctx, ctxCancelFunc = context.WithDeadline(context.Background(), timeUntilContextDeadline)
-	//	defer ctxCancelFunc()
-
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 	reconcileDroplets, err := reconcile.NewReconciler(ctx)
